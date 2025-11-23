da1ba1182544ab09e806a55d4742077bd36d0e87
Yin Khin <you@example.com>
2025-11-23 21:28:43 +0700

    push laravel new

--- config/cors.php
+++ config/cors.php
@@ -1,49 +1,35 @@
 <?php
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Cross-Origin Resource Sharing (CORS) Configuration
+     * |--------------------------------------------------------------------------
+     * |
+     * | Here you may configure your settings for cross-origin resource sharing
+     * | or "CORS". This determines what cross-origin operations may execute
+     * | in web browsers. You are free to adjust these settings as needed.
+     * |
+     * | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+     * |
+     */
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'http://localhost:3000',
         'http://localhost:3002',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:3002',
         'http://localhost:5173',
-        'https://frontend-react-zdh9p94ks-yin-khins-projects.vercel.app',
+        'https://frontend-react-q1gnl3hwz-yin-khins-projects.vercel.app',
         'https://frontend-react-sable.vercel.app',
         'https://frontend-react-git-main-yin-khins-projects.vercel.app',
-        'https://*.vercel.app',
-        'https://glistening-insight.up.railway.app',
     ],
-
     'allowed_origins_patterns' => [
-        '/https:\/\/frontend-react-[a-z0-9]+\.vercel\.app/',
-        '/.*\.vercel\.app/',
         '/https:\/\/.*\.vercel\.app/',
     ],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
-
 ];
